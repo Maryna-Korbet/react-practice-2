54c4c9a16aaa648567cc26c73260aad5436b9acc
Maryna-Korbet <m22v86k@gmail.com>
2024-04-04 23:16:07 -0400

    added isEmpty

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -11,12 +11,19 @@ export class Gallery extends Component {
     page: 1,
     photos: [],
     showBtn: false,
+    isEmpty: false,
   }
 
   componentDidUpdate(_, prevState) {
     const { searchText, page } = this.state;
     if (searchText !== prevState.searchText || page !==prevState.page) {
       ImageService.getImages(searchText, page).then(({photos, total_results}) => {
+        
+        if (!photos.length) {
+          this.setState({ isEmpty: true });
+          return;
+        }
+        
         this.setState(prevState => ({
           photos: [...prevState.photos, ...photos],
           showBtn : page < Math.ceil(total_results / 15)
@@ -25,22 +32,32 @@ export class Gallery extends Component {
     }    
   }
   
-  handleSubmit=(searchText)=>this.setState({searchText});
+  handleSubmit = (searchText) => {
+    this.setState({
+      searchText,
+      page: 1,
+      photos: [],
+      showBtn: false,
+      isEmpty: false,
+    });
+  }
 
   handleLoadMore=()=> {
     this.setState((prevState)=> ({page: prevState.page + 1})); 
   } 
 
   render() {
-    const { photos, showBtn } = this.state;
+    const { photos, showBtn, isEmpty } = this.state;
 
     return (
       <>
         <SearchForm handleSubmit={this.handleSubmit} />
 
-        {photos.length > 0 ? <ImagesGallery photos={photos} /> : <NoImages />}
+        {photos.length > 0 && <ImagesGallery photos={photos} />}
         
-        {showBtn && <LoadMoreBtn  onClick={this.handleLoadMore}/>}
+        {showBtn && <LoadMoreBtn onClick={this.handleLoadMore} />}
+        
+        {isEmpty && <NoImages />}
       </>
     );
   }
